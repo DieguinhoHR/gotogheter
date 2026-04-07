1d4a362fe908f3b1092316faa8bdf7777fa17d21
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2026-04-07 20:15:43 -0300

    first commit

---
+++ src/test/kotlin/br/com/agilworks/gotogheter/GotogheterApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.agilworks.gotogheter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GotogheterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
